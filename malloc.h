d4eff3a605b902dec6de5000e39345d3a244e37f
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-01-29 15:19:00 -0500

    Add: New file malloc.h

---
+++ malloc.h
@@ -0,0 +1,22 @@
+#ifndef _MALLOC_H_
+#define _MALLOC_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+/* ----- naive_malloc.c ----- */
+void *naive_malloc(size_t size);
+
+/* ----- malloc.c ----- */
+void *_malloc(size_t size);
+
+
+/* -----  -----*/
+/* -----  -----*/
+/* -----  -----*/
+
+#endif /* _MALLOC_H_ */

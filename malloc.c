1f74949778cbaa9781ef5b9ff5fdade843927a19
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-02-01 12:59:43 -0500

    Feat(Updated): commented function malloc.c

--- malloc.c
+++ malloc.c
@@ -1,6 +1,12 @@
 #include "malloc.h"
 
-
+/**
+ * first_time - Create the page and initialize memory and point
+ * to the start address
+ * @page: Variable to save the page
+ *
+ * Return: Pointer to the start address of the page
+ */
 void *first_time(ssize_t *page)
 {
 	void *first_chunk;
@@ -20,7 +26,16 @@ void *first_time(ssize_t *page)
 	return (first_chunk);
 }
 
-void *re_use(void *ptr, size_t *len, size_t block, ssize_t page)
+/**
+ * new_block - Allocate new pages on the heap
+ * @ptr: Pointer to allocated page
+ * @len: Number of pointers
+ * @block: size of block to allocate
+ * @page: Value of page
+ *
+ * Return: Pointer to allocated page
+ */
+void *new_block(void *ptr, size_t *len, size_t block, ssize_t page)
 {
 	void *next_block;
 	size_t tmp = 0;
@@ -47,6 +62,14 @@ void *re_use(void *ptr, size_t *len, size_t block, ssize_t page)
 	return (ptr);
 }
 
+
+/**
+ * _malloc - Allocate enough memory to store
+ * @size: Is the size needed to be allocated for the user
+ *
+ * Return: A pointer to the allocated memory that is suitably aligned
+ * for any kind of variable
+ */
 void *_malloc(size_t size)
 {
 	void *ptr;
@@ -77,7 +100,7 @@ void *_malloc(size_t size)
 		ptr = (char *)ptr + block_size;
 	}
 	if (!flag)
-		ptr = re_use(ptr, &len, block, page);
+		ptr = new_block(ptr, &len, block, page);
 	tmp_block = (block_t *)ptr;
 	tmp_block->start = first_chunk;
 	tmp_block->used = 1;

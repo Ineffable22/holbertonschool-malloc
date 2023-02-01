42073ad7cc3e0416eb2790e73d3cb6f76cb9d022
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-02-01 14:16:03 -0500

    Add: New file test/main_calloc.c

---
+++ test/main_calloc.c
@@ -0,0 +1,90 @@
+#include "malloc.h"
+
+/**
+ * str_test - Test the memory allocation with pointer to char type
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int str_test(void)
+{
+	char *str;
+	int i;
+
+	printf("==========String allocation test==========\n");
+	printf("Starting break is %p\n", sbrk(0));
+	for (i = 0; i < 10; i++)
+	{
+		void *chunk;
+		block_t *block;
+
+		str = _calloc(1, 10);
+		if (str == NULL)
+			return (EXIT_FAILURE);
+		strcpy(str, "Holberton");
+		str[9] = '\0';
+		printf("%p: %s, ", (void *)str, str);
+		chunk = (void *)(str - sizeof(size_t));
+		block = (block_t *)(str - sizeof(size_t) - sizeof(block_t));
+		printf("chunk addr: %p, ", (void *)chunk);
+		printf("size: %lu, ", *((size_t *)chunk));
+		printf("start: %p, ", block->start);
+		printf("used: %d, ", block->used);
+		printf("break: %p\n", sbrk(0));
+		// pmem(block, 40);
+	}
+	printf("Final break is %p\n\n", sbrk(0));
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * int_test - Test the memory allocation with pointer to integer type
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int int_test(void)
+{
+	int *num;
+	int i, j;
+
+	printf("==========Integer allocation test==========\n");
+	printf("Starting break is %p\n", sbrk(0));
+	for (i = 0; i < 10; i++)
+	{
+		void *chunk;
+
+		num = _calloc(4, 5);
+		if (num == NULL)
+			return (EXIT_FAILURE);
+		for (j = 0; j < 10; j++)
+			num[j] = j;
+		printf("%p: ", (void *)num);
+		for (j = 0; j < 10; j++)
+			printf("%d, ", num[j]);
+		chunk = (void *)(num - sizeof(size_t));
+		printf("chunk addr: %p, ", (void *)chunk);
+		printf("size: %lu, ", *((size_t *)chunk));
+		printf("break: %p\n", sbrk(0));
+	}
+	printf("Final break is %p\n\n", sbrk(0));
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * main - main program to test the memory allocation
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+	if (str_test() == EXIT_FAILURE)
+	{
+		fprintf(stderr, "str_test: Error\n");
+		return (EXIT_FAILURE);
+	}
+	if (int_test() == EXIT_FAILURE)
+	{
+		fprintf(stderr, "int_test: Error\n");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}

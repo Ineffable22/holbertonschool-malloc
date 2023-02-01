49ab5f126e5ee740690c8e873b4c048be205ec77
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-01-31 22:14:06 -0500

    Feat(Update): New prototype added

--- malloc.h
+++ malloc.h
@@ -5,8 +5,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/param.h>
 
+#define ALIGNMENT(size) (size + (8 - (size % 8)))
 
+typedef struct block_s
+{
+	void           *start;
+	char           used;
+} block_t;
 
 /* ----- naive_malloc.c ----- */
 void *naive_malloc(size_t size);
@@ -14,9 +21,13 @@ void *naive_malloc(size_t size);
 /* ----- malloc.c ----- */
 void *_malloc(size_t size);
 
+/* ----- free.c -----*/
+void _free(void *ptr);
 
-/* -----  -----*/
-/* -----  -----*/
-/* -----  -----*/
+/* ----- calloc.c -----*/
+void *_calloc(size_t nmemb, size_t size);
+
+/* ----- realloc.c -----*/
+void *_realloc(void *ptr, size_t size);
 
 #endif /* _MALLOC_H_ */

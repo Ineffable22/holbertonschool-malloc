84e67c9011955317c7a8fefd97e4d32c5dba9f5b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-02-04 02:50:09 -0500

    Feat(messages, logic and new implementation): Error messages fixed, new global variable created. Alignment equal to malloc implemented malloc.c

--- malloc.c
+++ malloc.c
@@ -3,22 +3,27 @@
 /**
  * first_time - Create the page and initialize memory and point
  * to the start address
- * @page: Variable to save the page
+ * @max_page: Variable to save the page
+ * @block:    Size of block to allocate
  *
  * Return: Pointer to the start address of the page
  */
-void *first_time(ssize_t *page)
+void *first_time(ssize_t *max_page, size_t block)
 {
-	(*page) = sysconf(_SC_PAGESIZE);
-	if ((*page) == -1)
+	ssize_t page = 0;
+
+	page = sysconf(_SC_PAGESIZE);
+	if (page == -1)
 	{
-		fprintf(stderr, "first_time: sysconf error");
+		fprintf(stderr, "Line %d | %s: sysconf error\n", __LINE__, __func__);
 		return (NULL);
 	}
-	FIRST_CHUNK = sbrk((*page));
+	for ((*max_page) = page; (size_t)(*max_page) < block; (*max_page) += page)
+	{}
+	FIRST_CHUNK = sbrk((*max_page));
 	if (FIRST_CHUNK == (void *)-1)
 	{
-		perror("first_time: sbrk error");
+		fprintf(stderr, "Line %d | %s: sbrk error\n", __LINE__, __func__);
 		return (NULL);
 	}
 	return (FIRST_CHUNK);
@@ -28,39 +33,41 @@ void *first_time(ssize_t *page)
  * new_block - Allocate new pages on the heap
  * @ptr:   Pointer to allocated page
  * @block: size of block to allocate
+ * @size:  Is the size needed to be allocated for the user
  * @page:  Value of page
  *
  * Return: Pointer to allocated page
  */
-void *new_block(void *ptr, size_t block, ssize_t page)
+void *new_block(void *ptr, size_t block, size_t size, ssize_t page)
 {
-	void *next_block;
 	size_t tmp = 0;
+	size_t new_page = 0;
 
 	/* Page value for the first time, otherwise the rest of the page */
-	tmp = LEN ? *(size_t *)ptr : (size_t)page;
-
-	next_block = ((char *)ptr + block);
-	while (tmp < block)
+	tmp = LEN ? AVAILABLE : (size_t)page;
+	page = sysconf(_SC_PAGESIZE);
+	if (page == -1)
 	{
-		page = sysconf(_SC_PAGESIZE);
-		if (page == -1)
-		{
-			fprintf(stderr, "naive_malloc_page: sysconf error");
-			return (NULL);
-		}
-		if (sbrk(page) == (void *)-1)
-		{
-			perror("naive_malloc_page: sbrk error");
-			return (NULL);
-		}
-		tmp += page;
+		fprintf(stderr, "Line %d | %s: sysconf error\n", __LINE__, __func__);
+		return (NULL);
+	}
+	while (tmp + new_page < block)
+		new_page += (size_t)page;
+
+	if (sbrk(new_page) == (void *)-1)
+	{
+		fprintf(stderr, "Line %d | %s: sbrk error\n", __LINE__, __func__);
+		return (NULL);
 	}
 	/* Assigns the rest of the page value in the next block */
-	*(size_t *)next_block = tmp - block;
+	AVAILABLE = tmp + new_page - block;
+
+	/* Pattern found, more information in "test/compare.c" */
+	if (size % 0x10 <= 8 && size > 0xF)
+		block = (size + ((8 - (size % 0x10)) + 8));
 
 	/* Assigns the current block size */
-	*(size_t *)((char *)ptr + 0x8) = block - METADATA;
+	*(size_t *)((char *)ptr + 0x8) = block;
 	return (ptr);
 }
 
@@ -73,20 +80,20 @@ void *new_block(void *ptr, size_t block, ssize_t page)
  */
 void *_malloc(size_t size)
 {
-	void *ptr;
+	void *ptr = NULL;
 	size_t block = ALIGNMENT(size) + METADATA;
-	size_t i, block_size, flag = 0;
+	size_t i = 0, block_size = 0, flag = 0;
 	ssize_t page = 0;
 
 	if ((ssize_t)size < 0)
 	{
+		fprintf(stderr, "Line %d | %s: ", __LINE__, __func__);
 		fprintf(stderr, "Can not allocate negative memory\n");
 		return (NULL);
 	}
-	/* Find the starting address */
-	if (!FIRST_CHUNK)
+	if (!FIRST_CHUNK) /* Find the starting address */
 	{
-		FIRST_CHUNK = first_time(&page);
+		FIRST_CHUNK = first_time(&page, block);
 		if (!FIRST_CHUNK)
 			return (NULL);
 	}
@@ -105,7 +112,7 @@ void *_malloc(size_t size)
 	}
 	/* New block if no previous free block is found */
 	if (!flag)
-		ptr = new_block(ptr, block, page);
+		ptr = new_block(ptr, block, size, page);
 	/* Assign 0 to the previous block size */
 	*(size_t *)ptr = 0;
 	/* Indicates with a bit that this block is being used */

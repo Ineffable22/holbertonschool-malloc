37348369228ed39a7890f7325e6667513f8cfb8e
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-02-04 02:52:16 -0500

    Feat(New test): Compare.c added, compare real malloc and own malloc test/compare.c

---
+++ test/compare.c
@@ -0,0 +1,87 @@
+#include "malloc.h"
+
+/**
+ * pmem - print mem
+ * @p: memory address to start printing from
+ * @bytes: number of bytes to print
+ *
+ * Return: nothing
+ */
+void pmem(void *p, unsigned int bytes)
+{
+	unsigned char *ptr;
+	unsigned int i;
+
+	ptr = (unsigned char *)p;
+	for (i = 0; i < bytes; i++)
+	{
+		if (i != 0)
+		{
+			printf(" ");
+		}
+		printf("%02x", *(ptr + i));
+	}
+	printf("\n");
+}
+
+/**
+ * str_test - Test the memory allocation with pointer to char type
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int str_test(void)
+{
+	char *str1, *str2, *text = "Holberton";
+	int i;
+	void *chunk1, *chunk2;
+
+	printf("==========String allocation test==========\n");
+	printf("Starting break is %p\n", sbrk(0));
+	for (i = 0; i < 10000; i++)
+	{
+		str1 = _malloc(i);
+		if (str1 == NULL)
+			return (EXIT_FAILURE);
+		strcpy(str1, text);
+		chunk1 = (void *)(str1 - sizeof(size_t));
+		str2 = malloc(i);
+		if (str2 == NULL)
+			return (EXIT_FAILURE);
+		strcpy(str2, text);
+		chunk2 = (void *)(str2 - sizeof(size_t));
+		if (*((size_t *)chunk1) != *((size_t *)chunk2))
+		{
+			printf("i => %d\n", i);
+			printf("size1: %lu\n", *((size_t *)chunk1));
+			printf("size2: %lu\n", *((size_t *)chunk2));
+		}
+		_free(str1);
+		_free(str2);
+	}
+	printf("Final break is %p\n\n", sbrk(0));
+	return (EXIT_SUCCESS);
+}
+/**
+ * Range 16-24	My result => 49		malloc result => 33
+ * Range 32-40	My result => 65		malloc result => 49
+ * Range 40-56	My result => 81		malloc result => 65
+ * Range 64-72	My result => 97		malloc result => 81
+ * Range 80-88	My result => 113	malloc result => 97
+ * pattern match => 
+ * if (size % 0x10 <= 8 && size > 0xF)
+ * 		block = (size + ((8 - (size % 0x10)) + 8));
+ */
+/**
+ * main - main program to test the memory allocation
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+	if (str_test() == EXIT_FAILURE)
+	{
+		fprintf(stderr, "str_test: Error\n");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}

a7b727155b41ac0159992b297f64b5d364231e3b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2023-02-08 14:34:21 -0500

    Feat(Update): malloc and free updated free.c

--- free.c
+++ free.c
@@ -8,12 +8,14 @@
  */
 void _free(void *ptr)
 {
-	void *size = 0;
+	void *next_ptr = NULL;
+	size_t size = 0;
 
 	if (ptr)
 	{
-		size = (char *)ptr - (METADATA / 2);
-		(*(size_t *)size)--;
-		*(size_t *)((char *)size - (METADATA / 2)) = (*(size_t *)size);
+		size = *(size_t *)((char *)ptr - (METADATA / 2)) - 1;
+		next_ptr = (char *)ptr + size;
+		*(size_t *)((char *)next_ptr - METADATA) = size;
+		(*(size_t *)((char *)next_ptr - (METADATA / 2)))--;
 	}
 }
